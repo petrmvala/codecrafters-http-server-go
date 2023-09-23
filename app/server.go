b4f31388e8e704237a94587027eb413ac4399d20
Petr Vála <petr.vala@kiwi.com>
2023-09-23 13:06:31 +0200

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -13,9 +13,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	var b []byte
+	_, err = conn.Read(b)
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+
 }
